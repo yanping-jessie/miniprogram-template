594c51676b8263694552b51c248d86e4b6132669
yanping <752789782@qq.com>
2020-04-14 09:57:03 +0800

    feat: 版本更新提示逻辑

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,20 @@
 export default {
   onLaunch: function() {
     console.log('App Launch');
+
+    const updateManager = uni.getUpdateManager();
+    updateManager.onUpdateReady(function(res) {
+      uni.showModal({
+        title: '更新提示',
+        content: '新版本已经准备好，是否重启应用？',
+        success(res) {
+          if (res.confirm) {
+            // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+            updateManager.applyUpdate();
+          }
+        },
+      });
+    });
   },
   onShow: function() {
     console.log('App Show');
